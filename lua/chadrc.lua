ca3b5a1ff179c7e6892476cce9032642cb6fe7bf
g24tk008 <g24tk008@kw06.kki.yamanashi.ac.jp>
2024-11-11 14:33:20 +0900

    Chagnes テーマやターミナルの大きさを変更

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,12 +5,25 @@
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+	theme = "nord",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+  term = {
+  winopts = { winhl = "Normal:term,WinSeparator:WinSeparator" },
+  sizes = { sp = 0.5, vsp = 0.5, ["bo sp"] = 0.5, ["bo vsp"] = 0.5 },
+  float = {
+    relative = "editor",
+    row = 0.05,
+    col = 0.05,
+    width = 0.9,
+    height = 0.8,
+    border = "single",
+    },
+  },
 }
 
+vim.o.shell = "zsh"
 return M
